1595f363a45fcc1f3413d2f8ce3edc3dc241132c
전가희 <krjgh@naver.com>
2026-03-07 06:33:33 +0900

    temp: PostgreSQL-docker, 관리자 계정 db 확인.

--- finance_project/back/project/settings.py
+++ finance_project/back/project/settings.py
@@ -100,13 +100,17 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres_db',        # YAML의 POSTGRES_DB
+        'USER': 'postgres_user',      # YAML의 POSTGRES_USER
+        'PASSWORD': 'postgres_password', # YAML의 POSTGRES_PASSWORD
+        'HOST': 'db',                 # YAML의 서비스 이름
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
